23fd7ab4a312901be5b63b0ea74ef4e81afe2a7b
Twaha Rahman <mahi6703890@gmail.com>
2020-09-29 17:10:26 +0600

    Refactor to re-export functions

--- src/fcc_inter_algos/mod.rs
+++ src/fcc_inter_algos/mod.rs
@@ -1,2 +1,5 @@
-pub mod array_diff;
-pub mod sum_of_range;
+mod array_diff;
+mod sum_of_range;
+
+pub use array_diff::array_diff;
+pub use sum_of_range::sum_in_range;
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod fcc_inter_algos;
 use fcc_inter_algos::array_diff;
 
 fn main() {
-    let diff = array_diff::array_diff(
+    let diff = array_diff(
         &["andesite", "grass", "dirt", "pink wool", "dead shrub"],
         &["diorite", "andesite", "grass", "dirt", "dead shrub"],
     );
