3c8c6783ee95b8ec5114aafc5ca1af2d305f1800
Twaha Rahman <mahi6703890@gmail.com>
2020-09-29 19:16:07 +0600

    Add pig latin finished algo

--- src/fcc_inter_algos/pig_latin.rs
+++ src/fcc_inter_algos/pig_latin.rs
@@ -1,4 +1,40 @@
 #[allow(dead_code)]
-pub fn pig_latin(str_to_process: &str) -> &str {
-    str_to_process
+pub fn pig_latin(str_to_process: &str) -> String {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    let mut starts_with_vowel = false;
+    for &vowel in vowels.iter() {
+        if str_to_process.starts_with(vowel) {
+            starts_with_vowel = true;
+            break;
+        }
+    }
+
+    if !starts_with_vowel {
+        let mut vowel_encountered = false;
+
+        let mut pre_vowel_chars: Vec<char> = Vec::new();
+        let mut post_vowelChars: Vec<char> = Vec::new();
+
+        for character in str_to_process.chars() {
+            println!("{}", character);
+            if vowels.contains(&character) {
+                vowel_encountered = true;
+            } else {
+                pre_vowel_chars.push(character);
+            }
+
+            if vowel_encountered {
+                post_vowelChars.push(character);
+            }
+        }
+
+        let first_part: String = post_vowelChars.iter().copied().collect();
+        let second_part: String = pre_vowel_chars.iter().copied().collect();
+
+        format!("{}{}ay", first_part, second_part)
+    } else {
+        let processed_strings = format!("{}way", str_to_process);
+        processed_strings
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod fcc_inter_algos;
 use fcc_inter_algos::pig_latin;
 
 fn main() {
-    let str_to_process = String::from("california");
+    let str_to_process = String::from("age");
 
     let processed = pig_latin(str_to_process.as_str());
     println!("The processed string is: {}", processed);
